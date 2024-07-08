f22cc6c23108259a86f331a981ecb7c037b4d0e5
nicolasparrilla <naparrilla2012@gmail.com>
2024-07-08 01:15:17 -0300

    Update endpoints.js

--- src/_mock/endpoints.js
+++ src/_mock/endpoints.js
@@ -31,4 +31,16 @@ http://localhost:4000/api/users/lists/
 {
     "userId": "6677afbc070e2a3bc00e6a28"
 }
+
+http://localhost:4000/api/users/forgot-password
+{
+    "email": "naparrilla2012@gmail.com"
+}
+
+http://localhost:4000/api/users/reset-password
+{
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2N2M5OGIyOTNjZDNmMjY3NDljNzRiMiIsImlhdCI6MTcyMDIwNjg4MSwiZXhwIjoxNzIwMjEwNDgxfQ.0VaCBhw5yk7OYZem7mR8-Wsq1TWhV-oJt6Jpf9codTM",
+    "newPassword": "password3"
+}
+
 */
